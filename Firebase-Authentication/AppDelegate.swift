fb6b366f3de03bd8f46bf1c3b5ea4f821bbd379f
Nicki Parker <NP204409@falmouth.ac.uk>
2019-02-16 18:33:48 +0000

    Make firebase authentication default page for signup

--- Firebase-Authentication/AppDelegate.swift
+++ Firebase-Authentication/AppDelegate.swift
@@ -24,10 +24,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
         if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
             return true
         }
+        // other URL handling goes here.
         return false
     }
 
